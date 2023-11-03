9c7826794f4c682503cabc99f8a620dce6f69a05
carlapr <carlaperola.823@outlook.com>
2023-11-03 12:44:51 -0300

    tentando implantar php e mysql

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario-rivest';
+
+ //   $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+//
+//   if ($conexao->connect_errno) {
+//      echo "Erro de conexão: " . $conexao->connect_error;
+//  } else {
+//       echo "Conexão bem-sucedida";
+//   }
+?>

9239c9e12a37506a540254bc0e23731642b34dba
Carla Araujo <142643525+CarlaPr@users.noreply.github.com>
2024-05-01 22:12:54 -0300

    historico de consultas php

---
+++ GestãoHospitalar/historico.php
@@ -0,0 +1,105 @@
+<?php
+   
+   include('protect.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+
+    @import url('https://fonts.googleapis.com/css?family=Poppins:400,700,900');
+
+    * {
+        margin: 0px;
+        padding: 0px; 
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+        list-style: none;
+        text-decoration: none;
+    }
+    .recuo{
+        margin-top: 10px;
+        background-color: #74AFB2;
+    }
+
+    .navegacao{
+        background-color: rgba(255, 255, 255, 0.904);
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 18px 40px;
+        box-shadow: 0 0.1rem 0.5rem #ccc;
+        width: 100%;
+    }
+
+    .logo {
+        width: 50px;
+        height: auto;
+    }
+
+    .navegacao h1{
+    font-size: 18px;
+    }
+
+    .nav-menu {
+        display: flex;
+        justify-content: center;
+        gap: 5rem;
+        list-style-type: none;
+    }
+
+    .nav-menu li a {
+        color: black;
+        font-size: 15px;
+        font-weight: 500;
+        text-decoration: none;
+        transition: all 0.5s;
+    }
+
+    .nav-menu li a:hover {
+        color: brown;
+    }
+
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #f0f0f0;
+    }
+
+    </style>
+
+</head>
+<body>
+    
+    <header>
+        <div class="recuo"></div>
+
+        <nav class="navegacao">
+
+             <img src="./componentes/imagens/logo2.png" alt="logo da empresa Morello com cores azuis" class="logo">
+
+            <h1>Bem vindo ao portal do paciente, <?php echo $_SESSION['nome']; ?>.</h1>
+
+            <ul class="nav-menu">
+                <li><a href="index.html">Nosso Hospital</a></li>
+                <li><a href="colocar depois o caminho daqui">Portal Empresarial</a></li>
+                <li><a href="logout.php">Sair da Conta</a></li>
+                
+            </ul>
+        </nav>
+    </header>
+
+    <div class="">
+
+    </div>
+
+    
+</body>
+</html>
